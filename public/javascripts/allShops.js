465ba9876c1df25da193e6114b6d0180095cb0a6
root <root@iZwz9i9dn3ri2xdsg03stuZ.(none)>
2017-07-25 15:32:35 +0800

    sure customer location and transfer http to https to adapt to googlemap api

--- public/javascripts/allShops.js
+++ public/javascripts/allShops.js
@@ -1,9 +1,42 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(40.760975, -73.979729),
-    zoom: 12
+    zoom: 2
   });
   var infoWindow = new google.maps.InfoWindow;
+
+  // get customer location
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+      };
+      var markerMe = new google.maps.Marker({
+          map: map,
+          position: pos,
+          label: {}
+      });
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } 
+  else {
+  // Browser doesn't support Geolocation
+  handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+  }
+
+
+
   var markers = document.getElementsByTagName('li');
   Array.prototype.forEach.call(markers, function(markerElem) {
     var name = markerElem.getAttribute('name');
