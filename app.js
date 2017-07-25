8cb5133cf44e87603a851f4c2cecc16a8752903d
root <root@iZwz9i9dn3ri2xdsg03stuZ.(none)>
2017-07-25 17:09:32 +0800

    add axios

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/public',express.static(__dirname + '/public'));
+app.use('/js',express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js',express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css',express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/axios',express.static(__dirname + '/node_modules/axios/dist/axios.min.js'));
 app.use('/', index);
 
 // catch 404 and forward to error handler
